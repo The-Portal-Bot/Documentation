009936f3326cd6ed5945626db065cea1b7c3f2c2
Ioannis Tsiakkas <iantsiakkas@gmail.com>
2021-03-16 07:52:51 -0700

    updated music player

--- portal-website/src/pages/index.js
+++ portal-website/src/pages/index.js
@@ -8,6 +8,84 @@ import styles from './styles.module.css';
 
 const urlInvite = 'https://discord.com/oauth2/authorize?client_id=704400876860735569&permissions=8&redirect_uri=http%3A%2F%2Fwww.localhost%3A4000%2Fpremium%2F&scope=bot';
 
+const features = [
+  {
+    title: '99% up time',
+    imageUrl: 'svg/uptime.svg',
+    description: (
+      <>
+        24/7 online, so you never miss a thing.
+      </>
+    ),
+  },
+  {
+    title: 'Auto cleanup',
+    imageUrl: 'svg/clean.svg',
+    description: (
+      <>
+        Portal respects your server and removes all
+        unnecessary messages to reduce clutter.
+      </>
+    ),
+  },
+  {
+    title: 'Fast',
+    imageUrl: 'svg/fast_time.svg',
+    description: (
+      <>
+        Coded with attention to be fast, robust and reliable.
+        Hosted online so it never missed a click.
+      </>
+    ),
+  },
+  {
+    title: 'Customisable',
+    imageUrl: 'svg/customisable.svg',
+    description: (
+      <>
+        Every server is different, so Portal is unique
+        for every one of them.
+      </>
+    ),
+  },
+  {
+    title: 'Anti Spam',
+    imageUrl: 'svg/spam.svg',
+    description: (
+      <>
+        Portal will warn members if they are spamming.
+        Commands that are prown to spamming have cooldowns
+        so that spamming is avoided.
+      </>
+    ),
+  },
+  {
+    title: 'Music Quality',
+    imageUrl: 'svg/music.svg',
+    description: (
+      <>
+        The easiest way to add music to your server. there
+        your are only one channel and one name away of your songs !
+      </>
+    ),
+  }
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
 function AddButton({ imageUrl, title, link }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
@@ -64,12 +142,6 @@ function Home() {
 
       <main className={clsx('hero--secondary', styles.heroBanner)}>
         <div className="container">
-          <p className={clsx(styles.buttons, styles.aligner)}>
-            <h1 className={clsx('hero__subtitle', styles.alignRight, styles.minMax)}>Portal can be the center of your server</h1>
-            <img className={clsx(styles.featureImage, styles.marginLeft, styles.marginRight)} src={logo} alt={title} />
-            <h1 className={clsx('hero__subtitle', styles.alignLeft, styles.minMax)}>and help you organise and grow it</h1>
-          </p>
-          <hr className={clsx('hero__subtitle', styles.horizontalLine)} />
           <p className={clsx('hero__subtitle', styles.marginRight)}>
             With Portal's <Link
               className={clsx(
@@ -94,6 +166,30 @@ function Home() {
               Text Interpreter
             </Link> is a program used to generate channel names.<br />
             You can create anything you want !
+          </p>
+        </div>
+      </main>
+
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+
+      <main className={clsx('hero--secondary', styles.heroBanner)}>
+        <div className="container">
+          <p className={clsx(styles.buttons, styles.aligner)}>
+            <h1 className={clsx('hero__subtitle', styles.alignRight, styles.minMax)}>Portal can be the center of your server</h1>
+            <img className={clsx(styles.featureImage, styles.marginLeft, styles.marginRight)} src={logo} alt={title} />
+            <h1 className={clsx('hero__subtitle', styles.alignLeft, styles.minMax)}>and help you organise and grow it</h1>
           </p>
         </div>
       </main>
